fcd6df99b3e83e36423b4fdd3b6042885bd9831e
Ibra <brahim.kor@educa.jcyl.es>
2023-12-20 18:27:09 +0200

    ob1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {DataService} from "./services/data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'obtest';
+  posts$: Observable<any[]>;
+
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.posts$ = this.dataService.getPosts()
+  }
 }
